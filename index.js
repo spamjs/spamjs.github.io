dd99f058f51d94f0fe7608a8b887ddc488f92317
Lalit Tanwar <lalit.tanwar07@gmail.com>
2015-10-12 15:25:31 +0530

    CDN server created

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 var static = require('node-static');
-var file = new static.Server();
+var file = new static.Server({
+  cache: 7200,
+  serverInfo: "myserver",
+  indexFile: "index.htm",
+  gzip: true,
+  "X-test" : "ok",
+  headers : {
+    'Connection' : 'keep-alive',
+    'Access-Control-Allow-Origin' : "*",
+    'Access-Control-Allow-Methods' : 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers' : 'content-type'
+  }
+});
 require('http').createServer(function(request, response) {
-  request.addListener('end', function() {
-    file.serve(request, response);
-  }).resume();
+  if((/\/(src|dist|build)\//).test(request.url)){
+    request.addListener('end', function() {
+      file.serve(request, response);
+    }).resume();
+  } else {
+    request.addListener('end', function() {
+      file.serveFile("index.html",200, {}, request, response);
+    }).resume();
+
+  }
+
 }).listen(process.env.PORT || 3000);
